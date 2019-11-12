9c5944403782143af4143761c36052ddea6f42fc
XxxxxYyyyy <v-bruhal@microsoft.com>
2019-11-12 11:18:29 +0000

    Automated push from build 89532

---
+++ 21.customization-plain-ui/precache-manifest.cb01a5d5a4bd6818aee1ec5c333a3301.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1511fa10b733178f037fd8d98e07021",
+    "url": "/BotFramework-WebChat/21.customization-plain-ui/index.html"
+  },
+  {
+    "revision": "25b347b43e74998b188e",
+    "url": "/BotFramework-WebChat/21.customization-plain-ui/static/js/2.e9af63ee.chunk.js"
+  },
+  {
+    "revision": "8b5b5e73d83a95f89837",
+    "url": "/BotFramework-WebChat/21.customization-plain-ui/static/js/main.1bfc10df.chunk.js"
+  },
+  {
+    "revision": "607f44fcff51377f6521",
+    "url": "/BotFramework-WebChat/21.customization-plain-ui/static/js/runtime~main.6c454de8.js"
+  }
+]);
--- packages/component/src/hooks/useStyleSet.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-
-import WebChatUIContext from '../WebChatUIContext';
-
-export default function useStyleSet() {
-  return [useContext(WebChatUIContext).styleSet];
-}
